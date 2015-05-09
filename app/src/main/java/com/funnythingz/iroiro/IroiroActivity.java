fd553ba93c0936db5b5c9e7a38ec1703d65fc32e
funnythingz <funnythingz69+github@gmail.com>
2015-05-09 13:05:44 +0900

    removed not use file

---
+++ app/src/main/java/com/funnythingz/iroiro/IroiroActivity.java
@@ -0,0 +1,17 @@
+package com.funnythingz.iroiro;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+public class IroiroActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iroiro);
+
+        GridView iroiroView = (GridView)findViewById(R.id.iroiroView);
+        //iroiroView.setAdapter(new HueAdapter(this));
+    }
+}

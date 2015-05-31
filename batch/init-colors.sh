dc8cfe096f981ff96b159d33fe0e5d1b6fef00a2
funnythingz <funnythingz69+github@gmail.com>
2015-05-31 10:37:23 +0900

    added batch

---
+++ batch/init-colors.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+curl -i -F "color[name]=deep-purple" -F "color[code]=#673ab7" -F "color[text_code]=#000000" iroiro.space/v1/colors
+curl -i -F "color[name]=indigo" -F "color[code]=#3f51b5" -F "color[text_code]=#000000" iroiro.space/v1/colors
+curl -i -F "color[name]=blue" -F "color[code]=#2196f3" -F "color[text_code]=#000000" iroiro.space/v1/colors
+curl -i -F "color[name]=light-blue" -F "color[code]=#03a9f4" -F "color[text_code]=#000000" iroiro.space/v1/colors
+curl -i -F "color[name]=cyan" -F "color[code]=#00bcd4" -F "color[text_code]=#000000" iroiro.space/v1/colors
+curl -i -F "color[name]=teal" -F "color[code]=#009688" -F "color[text_code]=#000000" iroiro.space/v1/colors
+curl -i -F "color[name]=green" -F "color[code]=#4caf50" -F "color[text_code]=#000000" iroiro.space/v1/colors
+curl -i -F "color[name]=light-green" -F "color[code]=#8bc34a" -F "color[text_code]=#000000" iroiro.space/v1/colors
+curl -i -F "color[name]=lime" -F "color[code]=#cddc39" -F "color[text_code]=#000000" iroiro.space/v1/colors
+curl -i -F "color[name]=yellow" -F "color[code]=#ffeb3b" -F "color[text_code]=#000000" iroiro.space/v1/colors
+curl -i -F "color[name]=amber" -F "color[code]=#ffc107" -F "color[text_code]=#000000" iroiro.space/v1/colors
+curl -i -F "color[name]=orange" -F "color[code]=#ff9800" -F "color[text_code]=#000000" iroiro.space/v1/colors
